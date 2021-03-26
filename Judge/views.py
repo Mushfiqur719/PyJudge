301bfa44d475c8c28a7f28ae19d7426a9f7c1df5
Mushfiqur Rahman <rahmanmushfiqur719@gmail.com>
2021-03-27 00:29:05 +0600

    Added Judge app for Operator app

--- Judge/views.py
+++ Judge/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def show_operator_home_page(request):
-    return render(request, 'Operator/home.html')
+def show_judge_home_page(request):
+    return render(request, 'Judge/home.html')
 
-def show_operator_info_page(request):
+def show_judge_info_page(request):
     return HttpResponse("My name is Operator 1")
--- PyJudge/urls.py
+++ PyJudge/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from Contestant import views as cviews
-from Operator import views as oviews
+from Judge import views as jviews
 from User import views as uviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contestant/', include('Contestant.urls')),
-    path('operator/', include('Operator.urls')),
+    path('judge/', include('Judge.urls')),
     path('profile/', uviews.profile),
     path('register/', uviews.register),
 ]
