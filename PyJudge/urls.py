4e720735630e6471660bf9394dc4010534f7c805
Mushfiqur Rahman <rahmanmushfiqur719@gmail.com>
2021-04-01 19:01:06 +0600

    Changes at april 1

--- PyJudge/urls.py
+++ PyJudge/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('judge/', include('Judge.urls')),
     path('profile/', uviews.profile),
     path('register/', uviews.register,name='register'),
-    path('',uviews.profile,name='home'),
-    path('login/',auth_views.LoginView.as_view(template_name='User/login.html'),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='User/logout.html'),name='logout'),
+    path('', uviews.profile, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='User/login.html'),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='User/logout.html'),name='logout'),
     #path('logout/',)
 ]
